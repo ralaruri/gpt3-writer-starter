a65f11053bd1ac1519d36b27342363cef3f7aa75
Ramzi A <ramzi.alaruri@gmail.com>
2022-12-11 22:42:04 -0500

    adding changes to file

---
+++ pages/api/generate.js
@@ -0,0 +1,32 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix =
+`
+Can you explain this SQL common table expression (also known as a CTE). 
+Explain each of the columns what they mean and the functions used in the columns. 
+Including what functions do after the from statement (group by, order by etc.)
+
+SQL:
+`;
+const generateAction = async (req, res) => {
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}\n`);
+
+  const baseCompletion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.7,
+    max_tokens: 250,
+  });
+
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
